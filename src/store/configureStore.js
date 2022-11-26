eb6c105867c3ec0a243b5678d7b23721fa9c6b63
Thomas Godwin <leddfoot36@gmail.com>
2022-11-26 14:38:12 +0100

    removed unnecessary log

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,7 +3,6 @@ import rootReducer from '../reducers/rootReducer';
 
 export default function configureStore() {
     const store = createStore(rootReducer);
-    console.log(store.getState());
     if (process.env.NODE_ENV !== 'production' && module.hot) {
         module.hot.accept('../reducers', () => store.replaceReducer(rootReducer));
     }
