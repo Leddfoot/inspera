e7cb19efd9a77ab72928f46ac77291adb621e708
Thomas Godwin <leddfoot36@gmail.com>
2022-11-26 12:41:25 +0100

    Removed unused imports, code, scss variables etc. & general cleanup

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,10 +1,6 @@
 import { createStore } from 'redux';
 import rootReducer from '../reducers/rootReducer';
 
-
-
-
-
 export default function configureStore() {
     const store = createStore(rootReducer);
     console.log(store.getState());
